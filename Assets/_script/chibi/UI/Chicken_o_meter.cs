d4e8d58d6a095a8188caa094d7c46ea93056486a
dem4ply <dem4ply@gmail.com>
2021-06-06 00:27:53 -0500

    init

---
+++ Assets/_script/chibi/UI/Chicken_o_meter.cs
@@ -0,0 +1,12 @@
+﻿namespace chibi.UI.chicken_o_meter
+{
+	public class Chicken_o_meter : Chibi_behaviour
+	{
+		chibi.tool.reference.Reference_gauge gauge;
+
+		protected override void _init_cache()
+		{
+			base._init_cache();
+		}
+	}
+}
